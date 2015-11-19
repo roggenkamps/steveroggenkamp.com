ed184d02e133b3fceca7d3c1568cdd6f54177e00
Steve Roggenkamp <roggenkamps@acm.org>
2015-11-19 12:20:48 -0500

    Got the homepage styled and working, finally

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,5 +4,13 @@ import Import
 
 getHomeR :: Handler Html
 getHomeR = do
+    blogPosts <- runDB $ selectList [] [LimitTo 3]
     defaultLayout $ do
         $(widgetFile "homepage")
+
+{-
+   where
+    selectArticles :: Text -> Handler [Entity BlogPost]
+    selectArticles t = runDB $ rawSql s [toPersistValue t]
+      where s = "SELECT ?? FROM blog_post where id = t"
+-}
