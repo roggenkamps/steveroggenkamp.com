418ccd3df8abe9def7f390a89bceec700cf1c135
Steve Roggenkamp <roggenkamps@acm.org>
2015-11-19 12:58:35 -0500

    More cleanup on the site for the articles

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,13 +4,6 @@ import Import
 
 getHomeR :: Handler Html
 getHomeR = do
-    blogPosts <- runDB $ selectList [] [LimitTo 3]
+    blogPosts <- runDB $ selectList [] [Desc BlogPostId, LimitTo 3]
     defaultLayout $ do
         $(widgetFile "homepage")
-
-{-
-   where
-    selectArticles :: Text -> Handler [Entity BlogPost]
-    selectArticles t = runDB $ rawSql s [toPersistValue t]
-      where s = "SELECT ?? FROM blog_post where id = t"
--}
