588eb12bd585e42529767dfdf09f2496b923b30d
Steve Roggenkamp <roggenkamps@acm.org>
2015-11-22 10:56:12 -0500

    Post editing now working

--- Handler/PostEdit.hs
+++ Handler/PostEdit.hs
@@ -4,33 +4,33 @@ import Import
 import Yesod.Form.Bootstrap3
 import Yesod.Text.Markdown
 import Data.Time.Format
-import Handler.Common
 import Database.Persist.Sql
 
-blogPostEditForm :: AForm Handler BlogPost
-blogPostEditForm = BlogPost 
-            <$> areq textField     (bfs ("Title" :: Text)) Nothing -- (Just "A Title")-- (blogPostTitle blogPost))
+blogPostEditForm :: BlogPost -> AForm Handler BlogPost
+blogPostEditForm blogPost = BlogPost 
+            <$> areq textField     (bfs ("Title" :: Text)) (Just (blogPostTitle blogPost))
             <*> pure ((parseTimeOrError True defaultTimeLocale "%Y-%m-%d"  "2015-11-21")::UTCTime)
             <*> lift (liftIO getCurrentTime)
-            <*> areq markdownField (bfs ("Article" :: Text)) Nothing -- (Just "An artile") --(blogPostArticle blogPost))
+            <*> areq markdownField (bfs ("Article" :: Text)) (Just (blogPostArticle blogPost))
 
 getPostEditR :: BlogPostId -> Handler Html
 getPostEditR  blogPostId  = do
     blogPost <- runDB $ get404 blogPostId
-    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostEditForm
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm $ blogPostEditForm blogPost
     defaultLayout $ do
         $(widgetFile "posts/edit")
 
 -- YesodPersist master => YesodPersistBackend master ~ SqlBackend => …
-{-
+
 postPostEditR ::  BlogPostId ->  Handler Html
 postPostEditR blogPostId = do
     blogPost <- runDB $ get404 blogPostId
-    ((res, widget), enctype)  <- runFormPostNoToken $ renderBootstrap3 BootstrapBasicForm blogPostEditForm
+    ((res, widget), enctype)  <- runFormPostNoToken $ renderBootstrap3 BootstrapBasicForm $ blogPostEditForm blogPost
     case res of
       FormSuccess editedBlogPost  -> do
-              blogPostDBId <- runDB $ insert editedBlogPost
-              redirect $ PostDetailsR blogPostDBId
+              blogPostDBId <- runDB $ update blogPostId [ BlogPostTitle =. (blogPostTitle editedBlogPost),
+                                                          BlogPostArticle =. (blogPostArticle editedBlogPost)]
+              redirect $ PostDetailsR blogPostId
       FormFailure errorMsgs -> defaultLayout $(widgetFile "errorpage")
       _                     -> defaultLayout $(widgetFile "posts/edit")
--}
+
